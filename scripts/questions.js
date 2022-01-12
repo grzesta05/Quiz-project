94f0cf69796b4e496791da6b9d3beadddf0882f0
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2022-01-12 12:39:44 +0100

    Added script files, reset css and created index page layout

---
+++ scripts/questions.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: "a",
+        answers: ["answer a", "answer b", "answer c", "answer d"],
+        correct_answer_id: 2
+    },
+    {
+        question: "a",
+        answers: ["answer a", "answer b", "answer c", "answer d"],
+        correct_answer_id: 2
+    }
+]
