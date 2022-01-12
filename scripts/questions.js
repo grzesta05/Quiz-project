552b7b7a2e96afeb5f70aa0b5e65e65fd15f56b8
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2022-01-12 20:56:14 +0100

    Layout project finished

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,11 +1,13 @@
 const questions = [
     {
-        question: "a",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
         question: "a",
+        question_background_url: "",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     }
