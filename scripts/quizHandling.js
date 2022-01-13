46d46d9b85b40e6fe48de2e378dbbbbd130b7de0
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2022-01-13 16:06:39 +0100

    Finished

--- scripts/quizHandling.js
+++ scripts/quizHandling.js
@@ -1,1 +1,85 @@
-console.log(questions[0].question);
+var points = 0;
+var timeTotal = 0;
+var timeForQuestion = 0;
+var QuestionsCounter = 0;
+var answeredCorrectly = 0;
+questionHandler();
+
+
+for(let i = 0; i < questions.length; i++)
+{
+    document.getElementById("progress-bar").innerHTML += `<div class="block" id="block${i}"></div>`;
+}
+//Function for more clean code - injecting li with answers 
+function answerInjector(answer, number)
+{
+    document.getElementById("answersSpace").innerHTML+=` <li class="answer" onclick="isCorrect(${number})">${answer}</li>`
+}
+
+function clearContainers()
+{
+    document.getElementById("time").innerHTML = "0:00";
+    document.getElementById("answersSpace").innerHTML = "";
+    document.getElementById("question").innerHTML = "";
+}
+
+function questionInjector(question)
+{
+    document.getElementById("question").innerHTML = question;
+}
+
+function theEndOfTheGame()
+{
+    console.log(points);
+    finished = true;
+    document.getElementById("time").innerHTML = "";
+    document.getElementById("question").innerHTML = 
+`Finished! <br /> Done ${answeredCorrectly} questions correctly out of ${questions.length} - ${Math.floor(100*answeredCorrectly/questions.length)}% <br /> Points: ${Math.floor(points)} <br /> Time: ${Math.floor(timeTotal/1000)} seconds`;
+}
+
+function isCorrect(answerNumber)
+{
+    answered = true;
+    if(answerNumber == questions[QuestionsCounter].correct_answer_id)
+    {
+        points += 200;
+        points += (800 / (1+ timeForQuestion/20000)); // max 800 points for time
+        answeredCorrectly++;
+        document.getElementById(`block${QuestionsCounter}`).style= `background-color: rgb(${255 - (255 * (800/(1+ timeForQuestion/5000))/800)},255,0);`;
+    }else{
+        document.getElementById(`block${QuestionsCounter}`).style= `background-color: rgb(255,0,0);`;  
+    }
+    QuestionsCounter++;
+    questionHandler();
+} 
+function questionHandler()
+{
+    const questionAsked = QuestionsCounter;
+    clearContainers();
+    if(QuestionsCounter == questions.length)
+    {
+        theEndOfTheGame();
+    }else{
+    const questionObj = questions[QuestionsCounter];
+    document.getElementsByClassName("main-activity")[0].style = `background-image: url(${questionObj.question_background_url});`;
+    questionInjector(questionObj.question);
+    for(let i = 0; i < questionObj.answers.length; i++)
+    {
+        answerInjector(questionObj.answers[i], i)
+    }
+    const time = Date.now();
+    var interval = window.setInterval(()=>{
+        if(QuestionsCounter != questionAsked){
+            window.clearInterval(interval);
+            timeTotal += Date.now() - time;
+        }else{
+        const minutes = Math.floor((Date.now()-time)/60000);
+        const seconds = Math.floor((Date.now()-time)/1000) - 60*Math.floor((Date.now()-time)/60000);
+        document.getElementById("time").innerHTML = `${minutes}:` + (seconds < 10? "0" : "") + `${seconds}`;
+        timeForQuestion = Date.now() - time;
+        }
+    }, 1000);
+    }
+}
+
+
