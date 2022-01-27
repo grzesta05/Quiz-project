2c2111f36223316c134b902a7bb944539144e133
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2022-01-27 21:49:25 +0100

    Updated layout

--- scripts/questions.js
+++ scripts/questions.js
@@ -6,104 +6,56 @@ const questions = [
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/7b1f8873-0486-4151-855d-364fd23d5955/southern-right-whale-nationalgeographic_1214903_16x9.jpg",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     },
     {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
-        answers: ["answer a", "answer b", "answer c", "answer d"],
-        correct_answer_id: 2
-    },
-    {
-        question: "a",
-        question_background_url: "https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids_16x9.jpg?w=1200",
+        question: "Baza wirusów programu avast została zaktualizowana",
+        question_background_url: "https://i.natgeofe.com/n/80b97fe3-c0fb-40a6-a6a6-88b5c3bb9d30/01-lion-populations-nationalgeographic_1777804.jpg",
         answers: ["answer a", "answer b", "answer c", "answer d"],
         correct_answer_id: 2
     }
