ed030f1ccbb130bd17fa646c548de40726cdfd87
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2022-02-02 11:35:24 +0100

    Last changes

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,60 +1,61 @@
 const questions = [
     {
         question: "Co jest oficjalną Maskotką Linuxa?",
-        question_background_url: "/img/img1.jpg",
+        question_background_url: "../img/img1.jpg",
         answers: ["Pingwin", "Słoń", "Surykatka", "Kruk"],
-        correct_answer_id: 0},
+        correct_answer_id: 0
+    },
     {
         question: "W jakim języku napisano Linuxa?",
-        question_background_url: "/img/img2.jpg",
+        question_background_url: "../img/img2.jpg",
         answers: ["C#", "C++", "C", "Pascal"],
         correct_answer_id: 2
     },
     {
         question: "Ile ważyło pierwsze jądro Linux?",
-        question_background_url: "/img/img3.jpg",
+        question_background_url: "../img/img3.jpg",
         answers: ["65 KB", "1 MB", "4 MB", "4 KB"],
         correct_answer_id: 0
     },
     {
         question: "Ile dystrybucji Linuxa jest aktywnie utrzymywanych?",
-        question_background_url: "/img/img4.jpg",
+        question_background_url: "../img/img4.jpg",
         answers: ["ok. 300", "ok. 200", "ok. 100", "mniej niż 50"],
         correct_answer_id: 0
     },
     {
         question: "Jak nazywa się najpopularniejsza dystrybucja Linuxa?",
-        question_background_url: "/img/img5.jpg",
+        question_background_url: "../img/img5.jpg",
         answers: ["Arch Linux", "Ubuntu", "Fedora", "openSUSE"],
         correct_answer_id: 1
     },
     {
         question: "Kto napisał Linuxa?",
-        question_background_url: "/img/img6.jpg",
+        question_background_url: "../img/img6.jpg",
         answers: ["Terry Davis", "Linus Torvalds", "Ken Thompson", "Bjarne Stroustrup"],
         correct_answer_id: 1
     },
     {
         question: "W którym roku wydany został pierwszy kod jądra Linux?",
-        question_background_url: "/img/img7.png",
+        question_background_url: "../img/img7.png",
         answers: ["1981", "1997", "1986", "1991"],
         correct_answer_id: 3
     },
     {
         question: "Ile linijek ma aktualnie kod jądra Linuxa?",
-        question_background_url: "/img/img8.jpg",
+        question_background_url: "../img/img8.jpg",
         answers: ["ok. 25 mln", "ok. 10 mln", "mniej niż 1 mln", "ok. 40 mln"],
         correct_answer_id: 0
     },
     {
         question: "Jak początkowo miał nazywać się Linux?",
-        question_background_url: "/img/img9.jpg",
+        question_background_url: "../img/img9.jpg",
         answers: ["Freax", "Linus", "Tux", "Unix"],
         correct_answer_id: 0
     },
     {
         question: "Na której dystrybucji opiera się ubuntu?",
-        question_background_url: "/img/img10.jpg",
+        question_background_url: "../img/img10.jpg",
         answers: ["Void", "Arch Linux", "Debian", "Gentoo"],
         correct_answer_id: 2
     }
